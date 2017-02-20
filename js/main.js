b4f3c159499139aa791cdf2f290c9856412e325c
emelyarellano1 <emelyac91@hotmail.com>
2017-02-20 15:53:42 -0300

    first commit prototype 1

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$('#about').typewrite({
+    cursor: '',
+  speed: 4,
+  actions: [
+    {type: 'About'},
+    {delay: 1500}
+  ]
+});
+
+$('#video').typewrite({
+    cursor: '',
+    speed: 4,
+  actions: [
+    {type: 'Video'},
+    {delay: 1500}
+  ]
+});
+
+$('#gallery').typewrite({
+    cursor: '',
+    speed: 4,
+  actions: [
+    {type: 'Gallery'},
+    {delay: 1500}
+  ]
+});
+
+$('#contact').typewrite({
+    cursor: '',
+    speed: 4,
+  actions: [
+    {type: 'Contact'},
+    {delay: 1500}
+  ]
+});
