667f39cc290218975fde86c2f616a3db563a5eb6
emelyarellano1 <emelyac91@hotmail.com>
2017-02-21 18:34:43 -0300

    fincionalidad de los plugin

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('#about').typewrite({
+$('#about-menu').typewrite({
     cursor: '',
   speed: 4,
   actions: [
@@ -7,7 +7,7 @@ $('#about').typewrite({
   ]
 });
 
-$('#video').typewrite({
+$('#video-menu').typewrite({
     cursor: '',
     speed: 4,
   actions: [
@@ -16,7 +16,7 @@ $('#video').typewrite({
   ]
 });
 
-$('#gallery').typewrite({
+$('#gallery-menu').typewrite({
     cursor: '',
     speed: 4,
   actions: [
@@ -25,7 +25,7 @@ $('#gallery').typewrite({
   ]
 });
 
-$('#contact').typewrite({
+$('#contact-menu').typewrite({
     cursor: '',
     speed: 4,
   actions: [
@@ -33,3 +33,62 @@ $('#contact').typewrite({
     {delay: 1500}
   ]
 });
+
+$(document).ready(function () { 
+  $('body').sectionScroll(); // Easy Peasy Lemon Squeezy 
+  // iniciando circull scroll en la seccion about us
+  $('html').csInit({
+
+    // global settings
+    wheelDelay: 300,
+    wheelDistance: 100,
+    wheelEase: 'easeOutQuad'
+    
+  });
+
+  $('#about').csTween({
+
+    // begin / end scroll position
+    begin: 0,
+    end: 300,
+
+    // custom CSS classes
+    from: {
+      letterSpacing: '0',
+      opacity: '1'
+    },
+    to: {
+        letterSpacing: '2vw',
+        opacity: '0'
+    },
+
+    // easing effect
+    easing: 'easeOutExpo',
+
+    // callbacks
+    onStart: function(el){
+      //console.log('onStart');
+    },
+
+    onProgress: function(el,p){
+      //console.log(p);
+    },
+
+    onComplete: function(el){
+      //console.log('onComplete');
+      $(el).hide();
+    },
+
+    onReverseStart: function(el){
+      //console.log('onReverseStart');
+      $(el).show();
+    },
+
+    onReverseComplete: function(el){
+      //console.log('onReverseComplete');
+    }
+
+  });
+
+
+}); 
